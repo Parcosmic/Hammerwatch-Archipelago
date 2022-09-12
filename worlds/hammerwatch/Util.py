0d5af41066efa8ec94f001b35ac7c9cc1ec8c62f
Joel Klecka <nomax45@gmail.com>
2022-09-11 23:51:25 -0500

    Added Cave Level 1 and Boss 1 levels, restructured location entries, added temporary regions

--- worlds/hammerwatch/Util.py
+++ worlds/hammerwatch/Util.py
@@ -5,6 +5,6 @@ class Counter:
     def __init__(self, start: int):
         self.counter = start
 
-    def count(self):
-        self.counter += 1
-        return self.counter - 1
+    def count(self, amount: int = 1):
+        self.counter += amount
+        return self.counter - amount
