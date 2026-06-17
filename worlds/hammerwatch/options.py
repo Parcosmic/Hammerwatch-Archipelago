6e2f04edb8267775150b241ce89b880e67385211
Parcosmic <111942824+Parcosmic@users.noreply.github.com>
2026-06-17 14:10:30 -0500

    Added faqs to docs

--- worlds/hammerwatch/options.py
+++ worlds/hammerwatch/options.py
@@ -131,7 +131,7 @@ class Buttonsanity(Choice):
 
 
 class ExitRandomization(Choice):
-    """Randomizes where level exits and portals lead
+    """Randomizes where level exits/portals lead. Use the /t command in-game to return to the start if you get stuck!
     No Boss Exits: exits to boss levels will not be shuffled
     All: all exits including bosses will be shuffled
     WARNING: This option has a moderate chance of causing generation to fail, use at your own risk!"""
